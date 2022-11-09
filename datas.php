44332354045d9303ae98bb90a1e1827fde0ba009
Wellington Vieira dos Santos <wellington.vsantos@gmail.com>
2022-11-09 17:23:10 -0300

    top git

---
+++ datas.php
@@ -0,0 +1,28 @@
+<?php 
+include 'config.php';
+$dataAtual = new DateTime();
+
+print_r($dataAtual);
+echo '<br>';
+echo $dataAtual->format('d/m/Y');
+echo '<br>';
+$dias = 30;
+for ($i=1; $i < 3; $i++) { 
+    echo $i.'ª parcela - '.(new DateTime('+'.$i.' month'))->format('d/m/Y');
+    $dias += 30; // $dias = dias + 30;
+    echo '<br>';
+}
+echo '<br>';
+$dataSistema = new DateTime();
+$dataNasc = new DateTime('2004/8/30');
+print_r($dataNasc);
+echo '<br>';
+$intervalo = $dataNasc->diff($dataSistema);
+print_r($intervalo); 
+echo '<br>';
+echo $intervalo->format('%y anos, %m meses e %d dias');
+
+
+
+
+?>
--- funcoes.php
+++ funcoes.php
@@ -1,3 +1,27 @@
 <?php 
+function geraCoeficienteParcela(float $taxa, int $periodo=1): float
+{
+    return pow((1+($taxa/100)),$periodo)/ $periodo;
+}
+function atrasoIntervalo($minutos)
+{
+   return $minutos>20?'Atrasado!':'Ok'; //if ternário
+}
+function validaMatricula(DateTime $data)
+{
+    $diff = $data->diff(new DateTime());
+    $anos = $diff->format('%y');
+
+    if ($anos < 6) {
+        return 'o garoto(a) não pode ser matric.';
+    }else{
+        return 'pode matricular o rebento!';
+    }
+}
+
+//echo validaMatricula(new DateTime('2010-6-23'));
+
+
+
 
 ?>
